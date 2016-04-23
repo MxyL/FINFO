dbb181acf13462c381c262b9e76d9d098193b8cc
MxyL <mxyL@live.ca>
2016-04-23 12:53:27 -0400

    'csv'

--- app.js
+++ app.js
@@ -8,6 +8,8 @@
 // for more info, see: http://expressjs.com
 var express = require('express');
 var Firebase = require("firebase");
+var fs = require('fs');
+var csv = require("csv-parse");
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -30,23 +32,24 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 });
 
 app.get('/test', function (req, res) {
-  res.send('GET request to the homepage');
-  parse();
+  // res.send('GET request to the homepage');
+  data = parse();
+  res.send(data);
 });
 
-var fs = require('fs');
-
 function store(data) {
-  var myDataRef = new Firebase('https://finfo.firebaseio.com/');
-  myDataRef.set(data);
+  var myDataRef = new Firebase('https://intense-fire-4574.firebaseio.com/');
+  myDataRef.set(data)
 }
 
 function parse(data) {
  
-  fs.readFile('./selection.csv', function (err, data) {
-  if (err) {
-    throw err; 
-  }
-  store(data);
-});
+  fs.readFile('./selection.csv', 'utf-8', function (err, data) {
+    if (err) {
+      return err; 
+    }
+    csv(data, function(err, output) {
+      store(output);
+    })
+  });
 }
