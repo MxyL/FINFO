083027b440c4cfb7005897b799d34d3b900e9c22
David Gao <jtgao20@gmail.com>
2016-04-23 16:52:12 -0400

    filtering.

--- app.js
+++ app.js
@@ -31,6 +31,32 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   console.log("server starting on " + appEnv.url);
 });
 
+var firebase = new Firebase('https://finfo1.firebaseio.com/');
+
+app.get("/catch", function(req, res) {
+  var ref = firebase.child("catch");
+  if(req.query.filter) {
+    if(req.query.filter.indexOf(">=") >= 0) {
+      ref = ref.orderByChild(req.query.filter.split(">=")[0]).startAt(req.query.filter.split(">=")[1]);
+    }
+    else if(req.query.filter.indexOf("<=") >= 0) {
+      ref = ref.orderByChild(req.query.filter.split("<=")[0]).endAt(req.query.filter.split("<=")[1]);
+    }
+    else if(req.query.filter.indexOf("=") >= 0) {
+      ref = ref.orderByChild(req.query.filter.split("=")[0]).equalTo(req.query.filter.split("=")[1]);
+    }
+  }
+
+  ref.once("value").then(function(snapShot) {
+    var val = snapShot.val();
+    var rtnVal = Object.keys(val).map(function(key) {
+      return val[key];
+    });
+    res.send(rtnVal);
+  })
+})
+
+
 app.get('/test', function (req, res) {
   // res.send('GET request to the homepage');
   data = parse();
@@ -38,15 +64,13 @@ app.get('/test', function (req, res) {
 });
 
 function store(data) {
-  var myDataRef = new Firebase('https://intense-fire-4574.firebaseio.com/');
-  myDataRef.push(data)
+  firebase.child("catch").push(data);
 }
 
 function parse(data) {
- 
   fs.readFile('./selection.csv', 'utf-8', function (err, data) {
     if (err) {
-      return err; 
+      return err;
     }
     csv(data, function(err, rows) {
       for (var i = 1; i < rows.length; i++) {
