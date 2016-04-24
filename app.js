01e73a237aa0e1848c9388ae09cfc2376264bc7e
MxyL <mxyL@live.ca>
2016-04-23 20:04:10 -0400

    pull

--- app.js
+++ app.js
@@ -8,8 +8,7 @@
 // for more info, see: http://expressjs.com
 var express = require('express');
 var Firebase = require("firebase");
-var fs = require('fs');
-var csv = require("csv-parse");
+var parsers = require("./parsers");
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -37,31 +36,7 @@ app.get('/test', function (req, res) {
   res.send(data);
 });
 
-function store(data) {
-  var myDataRef = new Firebase('https://intense-fire-4574.firebaseio.com/');
-  myDataRef.push(data)
-}
-
-function parse(data) {
- 
-  fs.readFile('./selection.csv', 'utf-8', function (err, data) {
-    if (err) {
-      return err; 
-    }
-    csv(data, function(err, rows) {
-      for (var i = 1; i < rows.length; i++) {
-        var tokens = rows[i];
-        store({
-            species: tokens[0],
-            date: tokens[1],
-            latitude: tokens[2],
-            longitude: tokens[3],
-            operation_id: tokens[4],
-            vessel: tokens[5],
-            total_catch_number: tokens[6],
-            total_catch_weight_kg: tokens[7]
-          });
-        }
-    })
-  });
-}
+app.get('/upload', function(req, res) {
+  parsers.parseDenmark();
+  res.send('GET request to the homepage');
+});
